c7467104b5284b41667279c576bc6ddc6af66427
Judy McGoldrick <jjeanius@verizon.net>
2018-05-05 14:32:53 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -51,6 +51,7 @@ class Dog
     sql = "SELECT * FROM dogs WHERE id = ?"
     result = DB[:conn].execute(sql, id)[0]
     Dog.new(result[0], result[1], result[2])
+    dog
   end
 
   def self::find_or_create_by
